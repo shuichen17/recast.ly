e612a10eb0d68018792b3906fc618c5735196f5b
shuichen17 <shuichen17@gmail.com>
2019-04-22 20:26:06 -0700

    component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var App = () => (
-  <div>
+
+
+
+
+
+
+
+
+
+    render() {
+    return (
+      <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -10,12 +24,32 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em><VideoList video = {exampleVideoData}/></h5></div>
       </div>
     </div>
   </div>
+    )
+    }
+  
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>videoPlayer</em> view goes here</h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><em>videoList</em> view goes here</h5></div>
+//       </div>
+//     </div>
+//   </div>
+// );
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
-var VideoList = () => (
+import exampleVideoData from '../data/exampleVideoData.js';
+
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.video.map((element) => 
+      <div>
+        <h5>
+          <em>videoListEntry</em>
+          <videoListEntry video = {element} />
+        </h5>
+      </div>
+    )}
   </div>
 );
 
@@ -17,3 +23,12 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+// var VideoList = () => (
+//   <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   </div>
+// );
